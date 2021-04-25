00e1972396b888bc3d83fb00b652678b8f9b7a21
Cheng Tianle <chengtianle1997@163.com>
2021-04-25 20:27:03 +0800

    structured binding and measure performance added

---
+++ structured_binding/structured_binding.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <tuple>
+#include <string>
+
+// Structured binding is a new feature in C++ 17.
+
+// return a tuple
+std::tuple<std::string, int> CreatePerson()
+{
+	return { "Tommy", 24 };
+}
+
+int main()
+{
+	std::tuple<std::string, int> person = CreatePerson();
+	std::string& person_name = std::get<0>(person);
+	int person_age = std::get<1>(person);
+
+	std::string name;
+	int age;
+	std::tie(name, age) = CreatePerson();
+
+	// structured binding: just keep returning a tuple
+	auto [name_t, age_t] = CreatePerson();
+	std::cout << name_t << ", " << age_t << std::endl;
+	// Remember to switch the compiler to C++ 17
+	// Project property -> C/C++ -> Language -> C++ Language Standard -> Switch to 'C++ 17'
+
+}
